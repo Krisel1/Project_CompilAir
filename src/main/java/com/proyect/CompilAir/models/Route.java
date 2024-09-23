366ca612c20d7261d035d2de12a2d2d92ac290aa
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-09-23 12:45:26 +0200

    model route

--- src/main/java/com/proyect/CompilAir/models/Route.java
+++ src/main/java/com/proyect/CompilAir/models/Route.java
@@ -1,9 +1,51 @@
 package com.proyect.CompilAir.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name)
+@Table(name = "routes")
 public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nameRoute")
+    private String nameRoute;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "route_id")
+    private Route route;
+
+    public Route(Long id, String nameRoute, Route route) {
+        this.id = id;
+        this.nameRoute = nameRoute;
+        this.route = route;
+    }
+
+    public Route() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameRoute() {
+        return nameRoute;
+    }
+
+    public void setNameRoute(String nameRoute) {
+        this.nameRoute = nameRoute;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
 }
