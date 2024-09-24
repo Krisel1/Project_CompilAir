26d85bb64431bd9e3933c2b801ae520d217478bb
Hilmar09 <hil.cris09@gmail.com>
2024-09-24 10:04:58 +0200

    mvc check

---
+++ src/main/java/com/proyect/CompilAir/excepcions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.proyect.CompilAir.excepcions;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/proyect/CompilAir/services/FlightService.java
+++ src/main/java/com/proyect/CompilAir/services/FlightService.java
@@ -1,4 +1,61 @@
 package com.proyect.CompilAir.services;
 
+import com.proyect.CompilAir.controllers.FlightController;
+import com.proyect.CompilAir.excepcions.ResourceNotFoundException;
+import com.proyect.CompilAir.models.Flight;
+import com.proyect.CompilAir.repositories.IFlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FlightService {
+
+    @Autowired
+    IFlightRepository iFlightRepository;
+
+    @Autowired
+    FlightController flightController;
+
+    public Flight createFlight(Flight flight) {
+        return iFlightRepository.save(flight);
+    }
+
+    public Flight updateFlight(Long id, Flight flightDetails) {
+        Flight existingFlight = iFlightRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Flight not found with id " + id));
+
+
+        existingFlight.setFlightName(flightDetails.getFlightName());
+        existingFlight.setRoute(flightDetails.getRoute());
+        existingFlight.setNumberOfSeats(flightDetails.getNumberOfSeats());
+
+
+        return iFlightRepository.save(existingFlight);
+    }
+
+    public List<Flight> getAllFlights() {
+        return iFlightRepository.findAll();
+    }
+
+    public Flight getFlightById(Long id) {
+        return iFlightRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Flight not found with id " + id));
+    }
+
+    public boolean deleteFlight(Long id) {
+        try {
+            Flight flight = iFlightRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Flight not found with id " + id));
+
+            iFlightRepository.delete(flight);
+            return true;
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
 }
+
+
