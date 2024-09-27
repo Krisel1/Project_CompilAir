5aa8c4addc888878010da1948ed7eb656253de66
krisel <kriselur@gmail.com>
2024-09-27 10:53:17 +0200

    File BookingDTO

--- src/test/java/com/proyect/CompilAir/controllers/FlightControllerTest.java
+++ src/test/java/com/proyect/CompilAir/controllers/FlightControllerTest.java
@@ -1,6 +1,5 @@
 package com.proyect.CompilAir.controllers;
 
-
 import com.proyect.CompilAir.models.Flight;
 import com.proyect.CompilAir.services.FlightService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,17 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-
 import org.springframework.test.web.servlet.MockMvc;
-
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-
-
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- src/test/java/com/proyect/CompilAir/controllers/RouteControllerTest.java
+++ src/test/java/com/proyect/CompilAir/controllers/RouteControllerTest.java
@@ -11,10 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.util.ArrayList;
 import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
