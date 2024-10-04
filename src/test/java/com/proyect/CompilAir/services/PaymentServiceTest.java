bb0590167ee1cea00b2cfe80a1830cf0fdec5ed1
Hilmar09 <hil.cris09@gmail.com>
2024-10-04 11:28:20 +0200

    test service payment

--- src/test/java/com/proyect/CompilAir/services/PaymentServiceTest.java
+++ src/test/java/com/proyect/CompilAir/services/PaymentServiceTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.proyect.CompilAir.services;
 
 import com.proyect.CompilAir.models.Booking;
@@ -83,7 +83,7 @@ public class PaymentServiceTest {
         when(bookingService.getBookingById(1L)).thenReturn(null);
 
         Exception exception = assertThrows(Exception.class, () -> {
-            paymentService.createPayment(1L, 100.00, "USD", "John Doe", "1234", LocalDateTime.now(), "Visa");
+            paymentService.createPayment(1L, 100.00, "USD", "Pedro Perez", "1234", LocalDateTime.now(), "Visa");
         });
 
         assertEquals("Booking not found", exception.getMessage());
@@ -125,84 +125,3 @@ public class PaymentServiceTest {
         verify(paymentRepository, times(1)).findById(1L);
     }
 }
-=======
-package com.proyect.CompilAir.services;
-
-
-import com.proyect.CompilAir.models.Booking;
-import com.proyect.CompilAir.models.Flight;
-import com.proyect.CompilAir.models.Payment;
-import com.proyect.CompilAir.repositories.PaymentRepository;
-import com.stripe.param.TopupListParams.Amount;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
-
-
-class PaymentServiceTest {
-
-    @InjectMocks
-    private PaymentService paymentService;
-
-    @Mock
-    private PaymentRepository paymentRepository;
-
-    private Payment payment;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-
-        //Object id;
-        payment = new Payment(1L,34,"ok","amparo",null,"ok","ok",null,null);
-    }
-    
-    @Test
-    public void createPayment() {
-        Payment newPayment = new Payment();
-        when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
-    
-        Payment createdPayment = paymentService.createPayment(newPayment);
-    
-        assertNotNull(createdPayment);
-        assertEquals("ok", createdPayment.getPaymentName());
-        verify(paymentRepository, times(1)).save(any(Payment.class));
-    }
-
-    @Test
-    public void getAllPayments() {
-        List<Payment> paymentList = Arrays.asList(payment);
-        when(paymentRepository.findAll()).thenReturn(paymentList);
-
-        List<Payment> payments = paymentService.getAllPayments();
-
-        assertEquals(1, payments.size());
-        assertEquals("ok", payments.get(0).getPaymentName());
-        verify(paymentRepository, times(1)).findAll();
-    }
-
-    @Test
-    public void getPaymentById() {
-        when(paymentRepository.findById(eq(1L))).thenReturn(Optional.of(payment));
-
-        Payment foundPayment = paymentService.getPaymentById(1L);
-
-        assertNotNull(foundPayment);
-        assertEquals("ok", foundPayment.getPaymentName());
-        verify(paymentRepository, times(1)).findById(eq(1L));
-    }
-}
-
-
->>>>>>> c11cdd011e6ebdc9e12193a4275d439fccfbad94
